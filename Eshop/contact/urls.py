60b1c2d66f919df4644c58f28f60fdb658aa4ae5
Amirhossein Alizadeh <amirhosseinalizadeh992@duck.com>
2025-08-15 17:38:52 +0330

    commit-17

--- Eshop/contact/urls.py
+++ Eshop/contact/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from .views import contact_us_page
+from .views import ContactUsPage
 
 urlpatterns = [
-    path('', contact_us_page)
+    path('', ContactUsPage.as_view(), name='contact')
 ]
--- Eshop/contact/views.py
+++ Eshop/contact/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from django.views import View
 
 # Create your views here.
 
-
-def contact_us_page(request):
-    return render(request, 'contact/contact_us_page.html', {})
+class ContactUsPage(View):
+    def get(self, request):
+        return render(request=request, template_name='contact/contact_us_page.html', context={})
+    
+    def post(self, request):
+        email = request.POST['email']
+        name = request.POST['name']
+        
+        print(email)
+        print(name)
+        return redirect(reverse('home'))
