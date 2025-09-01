95066726f16dd2a3ab46398261ced345dea36a1e
Amirhossein Alizadeh <amirhosseinalizadeh992@duck.com>
2025-09-01 16:43:24 +0330

    commit-31

--- Eshop/home/views.py
+++ Eshop/home/views.py
@@ -1,13 +1,39 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
+from site_settings.models import SiteSettings
 
 # Create your views here.
 class HomeView(TemplateView):
     template_name = 'home/home_index.html'
     
 def header_component(request):
-    return render(request=request, template_name="shared/site_header_component.html")
+    site_settings = SiteSettings.objects.filter(is_main_setting=True).first()
+    return render(
+        request=request, 
+        template_name="shared/site_header_component.html",
+        context={
+            "site_settings": site_settings
+        }
+    )
 
 
 def footer_component(request):
-    return render(request=request, template_name="shared/site_footer_component.html")
+    site_settings = SiteSettings.objects.filter(is_main_setting=True).first()
+    return render(
+        request=request, 
+        template_name="shared/site_footer_component.html",
+        context={
+            "site_settings": site_settings
+        }
+    )
+    
+    
+class AboutUsView(TemplateView):
+    template_name = "home/about_us.html"
+    
+    def get_context_data(self, **kwargs):
+        site_settings = SiteSettings.objects.filter(is_main_setting=True).first()
+        context = super().get_context_data(**kwargs)
+        context["site_settings"] = site_settings
+        return context
+    
