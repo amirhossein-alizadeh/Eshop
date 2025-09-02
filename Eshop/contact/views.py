c41ee1b4a5cc9f71d6c8bb9937c27e82613ede0f
Amirhossein Alizadeh <amirhosseinalizadeh992@duck.com>
2025-09-02 10:27:26 +0330

    commit-32

--- Eshop/contact/views.py
+++ Eshop/contact/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.views import View
 from django.views.generic.edit import FormView
+
+from site_settings.models import SiteSettings
 from .forms import ContactUsModelForm
 
 # Create your views here.
@@ -10,6 +12,11 @@ class ContactUsFormView(FormView):
     template_name = 'contact/contact_us_page.html'
     form_class = ContactUsModelForm
     success_url = '/contact-us'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        site_settings = SiteSettings.objects.filter(is_main_setting=True).first()
+        context["site_settings"] = site_settings
+        return context
     
     def form_valid(self, form):
         form.save()
