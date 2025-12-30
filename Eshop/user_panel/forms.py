8e29c91479e072e1a3de929544d9a6c19324c73d
Amirhossein Alizadeh <amirhosseinalizadeh992@duck.com>
2025-12-30 19:13:26 +0330

    commit-45

--- Eshop/user_panel/forms.py
+++ Eshop/user_panel/forms.py
@@ -12,3 +12,20 @@ class EditProfileModelForm(forms.ModelForm):
             "about_user",
             "address"
         ]
+        widgets = {
+            "first_name": forms.TextInput(attrs={"class": "form-control"}),
+            "last_name": forms.TextInput(attrs={"class": "form-control"}),
+            "avatar": forms.FileInput(attrs={"class": "form-control"}),
+            "about_user": forms.Textarea(
+                attrs={
+                    "rows": 5,
+                    "class": "form-control",
+                }
+            ),
+            "address": forms.Textarea(
+                attrs={
+                    "rows": 5,
+                    "class": "form-control",
+                }
+            )
+        }
--- Eshop/user_panel/views.py
+++ Eshop/user_panel/views.py
@@ -3,21 +3,33 @@ from django.views import View
 from django.views.generic import TemplateView
 
 from user_panel.forms import EditProfileModelForm
+from account.models import User
 
 
 class UserPanelView(TemplateView):
     template_name = "user_panel/user_panel.html"
 
 
-
 class EditProfileView(View):
     def get(self, request):
-        edit_profile_form = EditProfileModelForm()
+        current_user = User.objects.filter(id=request.user.id).first()
+        edit_profile_form = EditProfileModelForm(instance=current_user)
         context = {
-            "form": edit_profile_form
+            "form": edit_profile_form,
+            "current_user": current_user,
         }
         return render(request, template_name="user_panel/edit_profile.html", context=context)
 
+    def post(self, request):
+        current_user = User.objects.filter(id=request.user.id).first()
+        edit_profile_form = EditProfileModelForm(request.POST,request.FILES, instance=current_user)
+        if edit_profile_form.is_valid():
+            edit_profile_form.save()
+        context = {
+            "form": edit_profile_form,
+            "current_user": current_user,
+        }
+        return render(request, template_name="user_panel/edit_profile.html", context=context)
 
 def panel_component(request):
     return render(request, template_name="user_panel/components/user_panel_component.html")
